f6c5a2c2aff19a287a27aab1cabfa6fd9b102ff4
NINJAgur <edangur11@gmail.com>
2022-07-28 23:38:46 +0300

    28.7.22

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,34 @@
+<template>
+	<aside class="menu">
+        <p class="menu-label">
+            כללי
+        </p>
+
+        <ul class="menu-list">
+            <li><a class="button is-warning is-active">דשבורד</a></li>
+            <li><a>קורסים</a></li>
+        </ul>
+
+        <p class="menu-label">
+            ניהול
+        </p>
+
+        <ul class="menu-list">
+            <li><a>חופף</a></li>
+            <li><a>חפיפה</a></li>
+            <li><a>חומרי לימוד</a></li>
+            <li><a>אישורי צוות</a></li>
+            <li><a>אישורי קורסים</a></li>
+        </ul>
+
+        <p class="menu-label">
+            יצירת תוכן
+        </p>
+
+        <ul class="menu-list">
+            <li><a>הכשרות</a></li>
+            <li><a>מבחנים</a></li>
+            <li><a>שיעורים</a></li>
+        </ul>
+    </aside>
+</template>
--- frontend/src/views/dashboard/MyAccount.vue
+++ frontend/src/views/dashboard/MyAccount.vue
@@ -5,20 +5,28 @@
         <h1 class="title">המשתמש שלי</h1>
       </div>
     </div>
-    
-    <section class="section">
-      <div class="columns is-multiline">
-        <div class="column is-12">
+
+    <section class="section" style="direction: rtl">
+      <div class="columns is-multiline ">
+        <div class="column is-12 has-text-centered">
           <h2 class="subtitle is-size-3">הכשרות בביצוע</h2>
         </div>
-
-        <div 
-          class="column is-4"
-          v-for="course in courses"
-          v-bind:key="course.id"
-        >
-          <CourseItem :course="course" />
+        <div class="column is-3">
+            <Sidebar />
         </div>
+        <div class="column is-9">
+          <div class="columns is-multiline">
+            <div 
+              class="column is-4"
+              v-for="course in courses"
+              v-bind:key="course.id"
+            >
+              <CourseItem :course="course" />
+            </div>
+          </div>
+        </div>
+        
+        
       </div>
 
       <hr>
@@ -30,7 +38,7 @@
 
 <script>
 import axios from 'axios'
-
+import Sidebar from '@/components/Sidebar.vue'
 import CourseItem from '@/components/CourseItem.vue'
 
 export default {
@@ -40,11 +48,13 @@ export default {
       }
     },
     components: {
-      CourseItem
+      CourseItem,
+      Sidebar
     },
     mounted() {
         axios
-            .get('activities/get_active_courses/')
+            .get('courses/')
+            // .get('activities/get_active_courses/')
             .then(response => {
                 console.log(response.data)
 
